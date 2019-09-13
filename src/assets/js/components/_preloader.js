c65f864dae4b2c45501daa819fe061087c93f943
DmitriyCortY <dimaterlecskiu@gmail.com>
2019-09-13 22:44:09 +0300

    add adaptiviti on sections

--- src/assets/js/components/_preloader.js
+++ src/assets/js/components/_preloader.js
@@ -1,5 +1,20 @@
+if ($('.b-preloader').length > 0) {
+    $(".b-preloader__block").eq(0).fadeIn(580, function() {
+        $(".b-preloader__block").eq(0).fadeOut(580, function() {
+            $(".b-preloader__block").eq(1).fadeIn(580, function() {
+                $(".b-preloader__block").eq(1).fadeOut(580, function() {
+                    $(".b-preloader__block").eq(2).fadeIn(580)
+                })
+            })
+        })
+    })
+}
 $(document).ready(e => {
-    $(".b-preloader").fadeOut(400, function () {
-        $(".b-preloader").remove()
-    });
+    setTimeout(function() {
+        if ($('.b-preloader').length > 0) {
+            $(".b-preloader").fadeOut(400, function() {
+                $(".b-preloader").remove()
+            });
+        }
+    }, 3580)
 })
